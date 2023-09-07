8a107b7dddc8059bd41c0eff59e7b32a6e5378b9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-07 12:13:23 -0600

    add project registration eureka server

---
+++ src/main/java/com/registration_with_eureka/RegistrationWithEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.registration_with_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationWithEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationWithEurekaApplication.class, args);
+	}
+
+}
